c3854f983b230572cc7be406ba17b0708f7ecbb5
Đỗ Huy Hòa <dohuyhoa12012001gmail.com>
2024-05-21 22:53:55 +0700

    config cors client doimain

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify the allowed HTTP methods
     optionsSuccessStatus: 200,
     credentials: true, // Enable credentials (cookies, authorization headers, etc)
-    origin: ["http://localhost:3000", "http://localhost:3001"]
+    origin: ["https://client-ecomerce-three.vercel.app", "http://localhost:3001"]
 }));
 
 
